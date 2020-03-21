4a376ca0c30e715da96b6ba50eef40838f30bf87
V2Krishna <v2kris@gmail.com>
2020-03-20 22:49:53 -0500

    Added Arithmetic Operators Assignment Operators Comparision Operators

---
+++ 05_logicalOperators.py
@@ -0,0 +1,13 @@
+"""
+Logical Operators
+    and
+    or
+    not
+"""
+
+x = True
+y = False
+
+print("x and y is {}".format(x and y))
+print("x or y is {}".format(x or y))
+print("not of x is {}".format(not x))
