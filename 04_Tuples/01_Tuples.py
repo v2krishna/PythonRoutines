385e83f5a5b065652cf51ac4003b0f52d5724022
V2Krishna <v2kris@gmail.com>
2020-03-28 17:16:36 -0500

    Tuples Introduction

---
+++ 04_Tuples/01_Tuples.py
@@ -0,0 +1,14 @@
+"""
+    Tuples are one more data types
+    Once created , cannot be able to add, delete, replace  , reorder elements.
+    Sequence types
+    Holds any data type.
+    Tuples are immutables
+    Faster than lists.
+    Duplicate elements are allowed
+"""
+
+tuple_1 = (1, 2, 3, 4, 5, 6)
+print(tuple_1)
+
+print(len(tuple_1))
