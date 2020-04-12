a8d3e50f216518b8610995b068a7d164149608ab
V2Krishna <v2kris@gmail.com>
2020-04-12 00:45:40 -0500

    Tuples Introduction

--- 06_ControlStatements/05_oddnumbers.py
+++ 06_ControlStatements/05_oddnumbers.py
@@ -3,6 +3,7 @@ Display odd numbers between two numbers using for loop
 """
 m, n = [int(x) for x in input("Enter two numbers").split()]
 if m < n:
+    pass
 else:
     m, n = n, m
 print("Entered numbers are {0} and {1}".format(m, n))
